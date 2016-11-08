506db8fd9b6ed387e193d55547cfc2695bc201c0
jmatty1983 <jmatty1983@gmail.com>
2016-11-07 18:49:51 -0600

    Custom Config

---
+++ config/config.js
@@ -0,0 +1,77 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+var config = {
+	port: 8080,
+
+	language: 'en',
+	timeFormat: 24,
+	units: 'metric',
+
+	modules: [
+		{
+			module: 'alert',
+		},
+		{
+			module: 'clock',
+			position: 'top_left'
+		},
+	/*	{
+			module: 'calendar',
+			header: 'US Holidays',
+			position: 'top_left',
+			config: {
+				calendars: [
+					{
+						symbol: 'calendar-check-o ',
+						url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
+					}
+				]
+			}
+		},*/
+		{
+			module: 'compliments',
+			position: 'lower_third'
+		},
+		{
+			module: 'currentweather',
+			position: 'top_right',
+			config: {
+				location: 'Milwaukee',
+				locationID: '5263045',  //ID from http://www.openweathermap.org
+				appid: '84c49082b0c14ef92e632453e0b00e6d'
+			}
+		},
+		{
+			module: 'weatherforecast',
+			position: 'top_right',
+			header: 'Weather Forecast',
+			config: {
+	            location: 'Milwaukee',
+				locationID: '5263045',  //ID from http://www.openweathermap.org
+	            appid: '84c49082b0c14ef92e632453e0b00e6d'
+			}
+		},
+		{
+			module: 'newsfeed',
+			position: 'bottom_bar',
+			config: {
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== 'undefined') {module.exports = config;}
